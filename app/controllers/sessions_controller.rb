c157e3dbdb37a3404a1213be1089cfa87932bcde
danielmonr <danielmonr@gmail.com>
2015-05-01 12:50:11 -0500

    logged_ins?

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -39,5 +39,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,4 +3,32 @@ module SessionsHelper
 		session[:user_id] = user.id
 	end
 
+	def log_out
+		session.delete(:user_id)
+	end
+
+	def current_profesor
+		@current_profesor ||= Profesor.find_by(id: session[user_id])
+	end
+
+	def current_director
+		@current_director ||= Direcor.find_by(id: session[user_id])
+	end
+
+	def current_alumno
+		@current_alumno ||= Alumnos.find_by(id: session[user_id])
+	end
+
+	def alumno_logged_in?
+		!current_alumno.nil?
+	end
+
+	def profesor_logged_in?
+		!current_profesor.nil?
+	end
+
+	def director_logged_in?
+		!current_director.nil?
+	end
+
 end
