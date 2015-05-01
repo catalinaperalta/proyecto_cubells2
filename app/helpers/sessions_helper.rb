21a9fa73724554a17698fb0b0c20f20df89700b5
danielmonr <danielmonr@gmail.com>
2015-05-01 15:13:22 -0500

    ?

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -43,7 +43,7 @@ module SessionsHelper
 	end
 
 	def log_out
-		session.delete(:user_id)
+		session.delete(:user_id, :tipo)
 		@current_director = nil
 		@current_alumno = nil
 		@current_profesor = nil
