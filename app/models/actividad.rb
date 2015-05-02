14a57d32427758a07155c6dea2081ef57d503258
danielmonr <danielmonr@gmail.com>
2015-05-01 22:32:52 -0500

    algunas validaciones

--- app/models/actividad.rb
+++ app/models/actividad.rb
@@ -1,3 +1,6 @@
 class Actividad < ActiveRecord::Base
 	belongs_to :parcial
+	validates :nombre, presence: true, length: {maximum: 50}
+	validates :descripcion, length: {maximum: 50}
+	validates :calificacion, numericality: { only_integer: true, message: "Solo numeros enteros"}
 end
--- app/models/alumno.rb
+++ app/models/alumno.rb
@@ -2,4 +2,11 @@ class Alumno < ActiveRecord::Base
 	belongs_to :carrera
 	has_many :materia_alumnos
 	has_many :cursos, :through => :materia_alumnos
+
+	validates :matricula, presence: true, length: 9, uniqueness: true
+	validates :nombre, presence: true, length: {maximum: 50}
+	validates :apellido_p, presence: true, length: {maximum: 50}
+	validates :apellido_m, presence: true, length: {maximum: 50}
+	validates :password, presence: true, length: {minimum: 5, maximum:25}
+
 end
