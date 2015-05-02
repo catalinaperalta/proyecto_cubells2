cee8f573b27654ded99e641883381d1500a078d7
danielmonr <danielmonr@gmail.com>
2015-05-01 22:42:37 -0500

    correccion de validacion length

--- app/models/alumno.rb
+++ app/models/alumno.rb
@@ -3,7 +3,7 @@ class Alumno < ActiveRecord::Base
 	has_many :materia_alumnos
 	has_many :cursos, :through => :materia_alumnos
 
-	validates :matricula, presence: true, length: 9, uniqueness: true
+	validates :matricula, presence: true, length: {minimum: 9, maximum: 9}, uniqueness: true
 	validates :nombre, presence: true, length: {maximum: 50}
 	validates :apellido_p, presence: true, length: {maximum: 50}
 	validates :apellido_m, presence: true, length: {maximum: 50}
