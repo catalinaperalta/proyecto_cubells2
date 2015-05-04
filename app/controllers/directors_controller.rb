f78fa02b1209501a9c6cd45a5c44d7b2617cb4f9
danielmonr <danielmonr@gmail.com>
2015-05-04 14:32:20 -0500

    correccion de indicador de materias

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -13,7 +13,8 @@ class DirectorsController < ApplicationController
   def show
     @director = Director.find(params[:id])
     @numero_alumnos = Alumno.count(:conditions => ["carrera.id_director = ?", params[:id]], :joins => "JOIN carreras ON carreras.id = alumnos.id_carrera")
-    @numero_materias = MateriaCarrera.count(:conditions => ["carreras.id_director = ?", params[:id]], :joins => "JOIN carreras ON carreras.id = materia_carreras.carrera_id")
+    #@numero_materias = MateriaCarrera.count(:joins => "JOIN carreras ON carreras.id = materia_carreras.carrera_id", :conditions => ["carreras.id_director = ?", params[:id]])
+    @numero_materias = Curso.count(:join => "JOIN materia_carreras ON cursos.id = materia_carreras.materia_id JOIN carreras ON materia_carreras.carrera_id = carreras.id", :conditions => ["carreras.id_director = ?", params[:id]])
   end
 
   def listaa
